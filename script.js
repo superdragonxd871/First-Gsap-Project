ffd3fad9713f1196a2e60e3e0b9cc7197c6a1bb3
Sourabh <onlyysourabh871@gmail.com>
2026-01-06 01:56:58 +0530

    Add main4 & sometuning & Add smoothscroll

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger)
   // gsap code here!
 
+  const lenis = new Lenis({
+    lerp: 0.05,          // Webflow-like
+    smoothWheel: true
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    ScrollTrigger.update();
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+
   let tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".main1",
@@ -12,7 +24,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
       scrub: 1,
     }
 
-    
+
   });
 
   tl.to("nav", {
@@ -22,84 +34,108 @@ document.addEventListener("DOMContentLoaded", (event) => {
   tl.to(".section1 img", {
     scale: 1.18,
     // opacity:0,
-  },'same');
+  }, 'same');
 
   tl.to(".main1", {
     backgroundColor: " rgb(0, 0, 0)"
-  },'same');
+  }, 'same');
 
-  tl.to(".down1para",{
-    scale:1,
-  },'same');
-  
-  tl.to(".backblur",{
+  tl.to(".down1para", {
+    scale: 1,
+  }, 'same');
+
+  tl.to(".backblur", {
     // filter:"blur(0px)",
-    opacity:0,
-  },'same2');
-  
+    opacity: 0,
+  }, 'same2');
+
   tl.to(".section1 img", {
-    opacity:0,
-  },'same2');
-  
-  
-  tl.to(".section1text h2",{
-    color:"rgb(0,0,0)"
-  },'same2')
-  
-  tl.to(".section1text ul",{
-    color:"rgb(0,0,0)"
-  },'same2')
-  
-  tl.to(".section1text h1",{
-    scale:1.2,
-    filter:"blur(5px)",
-    opacity:0,
+    opacity: 0,
+  }, 'same2');
+
+
+  tl.to(".section1text h2", {
+    color: "rgb(0,0,0)"
+  }, 'same2')
+
+  tl.to(".section1text ul", {
+    color: "rgb(0,0,0)"
+  }, 'same2')
+
+  tl.to(".section1text h1", {
+    scale: 1.2,
+    filter: "blur(5px)",
+    opacity: 0,
   })
-  
-  
-  tl.to(".down1para",{
-    scale:0.8,
-    opacity:0,
+
+
+  tl.to(".down1para", {
+    scale: 0.8,
+    opacity: 0,
   });
 
-  let tl2=gsap.timeline({
+  let tl2 = gsap.timeline({
     scrollTrigger: {
       trigger: ".main2",
       start: "0% 100%",
-      end: "30% 70%",
+      end: "30% 50%",
       // markers: true,
       // pin: true,
       scrub: 1,
     }
   });
 
-  tl2.to(".innermain",{
-    scale:1,
+  tl2.to(".innermain", {
+    scale: 1,
+    opacity:1,
   })
 
-  let tl3=gsap.timeline({
-    scrollTrigger:{
-      trigger:".main3",
-      start:"30% 68%",
-      end:"80% 80%",
+  let tl3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".main3",
+      start: "30% 68%",
+      end: "100% 80%",
       // markers:true,
-      scrub:2,
+      scrub:1,
     }
   })
 
-  tl3.to(".main3img ",{
-    x:"-5%"
-  },'same')
+  tl3.to(".main3img ", {
+    x: "-5%"
+  }, 'same')
 
-  tl3.to(".main3img :nth-child(1)",{
-    opacity:1,
-  },'same')
+  tl3.to(".main3img :nth-child(1)", {
+    opacity: 1,
+  }, 'same')
 
 
-  tl3.to(".main3img :nth-child(1)",{
-    x:"11%"
+  tl3.to(".main3img :nth-child(1)", {
+    x: "11%"
   })
 
-  
+  tl3.to(".main3", {
+    backgroundColor: "#ffff",
+  }, 'same1')
+
+
+  tl3.to(".main3 .bottomtext", {
+    color: "rgb(0,0,0)"
+  }, 'same1')
+
+  let tl4 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".main4",
+      // markers: true,
+      start: "0% 80%",
+      end: "100% 10%",
+      scrub:1,
+    }
+  })
+
+  tl4.to(".para", {
+    x: "-60%"
+  })
+
+
 
 });
